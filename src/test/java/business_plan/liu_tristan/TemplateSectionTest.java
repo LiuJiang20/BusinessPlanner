e66233be2a027729d006627e0621427a5364df4a
Tristan <Tristan@10.84.16.31>
2019-02-26 19:48:44 -0500

    Updated TemplateSectionTest

--- src/test/java/business_plan/liu_tristan/TemplateSectionTest.java
+++ src/test/java/business_plan/liu_tristan/TemplateSectionTest.java
@@ -1,12 +1,13 @@
 package business_plan.liu_tristan;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class TemplateSectionTest
 {
-	void testConstruct()
+	static void testConstruct()
 	{
 		TemplateSection section1 = new TemplateSection("Mission", "Mission 1");
 		assert section1.category == "Mission";
@@ -19,7 +20,7 @@ class TemplateSectionTest
 		assert section2.childLimit == 5;
 	}
 	
-	void testEquals()
+	static void testEquals()
 	{
 		TemplateSection section1 = new TemplateSection("Vision", "Vision", 1);
 		TemplateSection section2 = new TemplateSection("Mission", "Mission 1", 1);
@@ -29,81 +30,107 @@ class TemplateSectionTest
 		TemplateSection section6 = new TemplateSection("Objective", "Objective 3", 7);
 		
 		assert section1.equals(section2) == false;
-		assert section2.equals(section3) == False;
-		assert section4.equals(section5) == True;
-		assert section5.equals(section6) == False;
+		assert section2.equals(section3) == false;
+		assert section4.equals(section5) == true;
+		assert section5.equals(section6) == false;
 		
 		section6.setName("Objective 1");
 		section6.setChildLimit(5);
-		assert section6.equals(section5) == True;
-		assert section4.equals(section6) == True;
+		assert section6.equals(section5) == true;
+		assert section4.equals(section6) == true;
 	}
-
-//	@Test
-//	void test()
-//	{
-//		fail("Not yet implemented");
-//	}
-//	
-//	void testEquals()
-//	{
-//		//Initialize a series of sections
-//		TemplateSection section1 = new TemplateSection("Vision", "Vision");
-//		TemplateSection section2 = new TemplateSection("Mission", "Mission");
-//		TemplateSection child = new TemplateSection("Child", "Child");
-//		
-//		section1.setChildLimit(1);
-//		section2.setChildLimit(1);
-//		
-//		section1.addChild(child);
-//		section2.addChild(child);
-//		
-//		assertEquals(section1, section1);
-//		assertNotEquals(section1, section2);
-//		
-//		section2.setCategory("Vision");
-//		assertNotEquals(section1, section2);
-//		
-//		section2.setName("Vision");
-//		assertNotEquals(section1, section2);
-//		
-//		section2.setChildLimit(1);
-//		assertEquals(section1, section2);
-//		
-//		TemplateSection child2 = new TemplateSection("Child2", "Child2");
-//		section2.deleteChild("Child");
-//		section2.addChild(child2);
-//		assertNotEquals(section1, section2);
-//		
-//		// more test needed for content
-//		
-//	}
-//	@Test
-//	void testDeepCopy()
-//	{
-//		TemplateSection section1 = new TemplateSection("Vision", "Vision");
-//		TemplateSection section2 = new TemplateSection("Mission", "Mission");
-//		TemplateSection section3 = new TemplateSection("Objective", "Objective");
-//		TemplateSection section4 = new TemplateSection("Strategy", "Strategy");
-//		
-//		section1.setChildLimit(1);
-//		section2.setChildLimit(1);
-//		section3.setChildLimit(5);
-//		section4.setChildLimit(1);
-//		
-//		section1.addChild(section2);
-//		section2.addChild(section3);
-//		section3.addChild(section4);
-//		
-//		TemplateSection sections[] = {section1,section2,section3,section4};
-//		for(TemplateSection section : sections)
-//		{
-//			TemplateSection deepCopy = section.deepCopy();
-//			assertEquals(section, deepCopy);
-//			assert section != deepCopy;
-//		}
-//		
-//		
-//		
-//	}
+	
+	static void testDeepCopy()
+	{
+		TemplateSection section1 = new TemplateSection("Objective", "Objective 1");
+		TemplateSection section2 = section1.deepCopy();
+		assert section1.equals(section2) == true;
+	}
+	
+	static void testContent()
+	{
+		TemplateSection section1 = new TemplateSection("Objective", "Objective 1");
+		assert section1.contents.size() == 0;
+		Text c = new Text("hello");
+		section1.addContent(c);
+		assert section1.contents.size() == 1;
+		try {
+			section1.deleteContent(c);
+		} catch (ContentNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assert section1.contents.size() == 0;
+	}
+	
+	static void testChild()
+	{
+		TemplateSection section1 = new TemplateSection("Objective", "Objective 1");
+		assert section1.children.size() == 0;
+		TemplateSection c = new TemplateSection("Action Plan", "Action Plan 1");
+		try {
+			section1.addChild(c);
+		} catch (ChildLIimitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assert section1.children.size() == 1;
+		try {
+			section1.deleteChild(c);
+		} catch (ChildNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assert section1.children.size() == 0;
+	}
+	
+	static void testCategoryName()
+	{
+		TemplateSection section1 = new TemplateSection("Objective", "Objective 1");
+		assert section1.getCategory() == "Objective";
+		section1.setCategory("Mission");
+		assert section1.getCategory() == "Mission";
+		assert section1.getName() == "Objective 1";
+		section1.setName("Mission");
+		assert section1.getName() == "Mission";
+	}
+	
+	static void testParent()
+	{
+		TemplateSection section1 = new TemplateSection("Objective", "Objective 1");
+		assert section1.getParent() == null;
+		TemplateSection section2 = new TemplateSection("Objective", "Objective 1");
+		section1.setParent(section2);
+		assert section1.getParent() == section2;
+	}
+	
+	static void testChildLimit()
+	{
+		TemplateSection section1 = new TemplateSection("Objective", "Objective 1");
+		assert section1.getChildLimit() == 1;
+		section1.setChildLimit(2);
+		assert section1.getChildLimit() == 2;
+	}
+	
+	static void testGetContents()
+	{
+		TemplateSection section1 = new TemplateSection("Objective", "Objective 1");
+		assert section1.getContents().size() == 0;
+		Text c = new Text("hello");
+		section1.addContent(c);
+		assert section1.getContents().size() == 1;
+	}
+	
+	public static void main(String[] args)
+	{
+		testConstruct();
+		testEquals();
+		testDeepCopy();
+		testContent();
+		testChild();
+		testCategoryName();
+		testParent();
+		testChildLimit();
+		testGetContents();
+	}
 }
